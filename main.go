d7b9dd70aa37b1ae85b800abf2289f33d3a2ded8
Zach C <zcarlson@signifai.io>
2017-03-29 19:09:02 -0400

    Some test main.go initial commit didn't cover

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"./nagios"
+	"fmt"
+	//	"io"
+	//	"os"
+)
+
+func main() {
+	/*
+		fd, err := os.Open("someFile.txt")
+		var readBuffer []byte = make([]byte, 512)
+		var BytesRead int
+
+		for BytesRead, err = fd.Read(readBuffer); err != nil && err != io.EOF; BytesRead, err = fd.Read(readBuffer) {
+			fmt.Print(string(readBuffer[0:BytesRead]))
+		}
+		// One last output because EOF terminates the for
+		fmt.Print(string(readBuffer[3:5]))
+
+		if err != nil {
+			fmt.Println("Got err", err)
+		}
+
+		fmt.Println(BytesRead, "bytes read")
+	*/
+	hoststatuses, servicestatuses, err := nagios.NagiosStatusMaps("/usr/local/nagios/var/status.dat")
+	if err == nil {
+		fmt.Println("Host statuses:")
+		for host, hostStatus := range hoststatuses {
+			fmt.Println("  ", host+":", hostStatus)
+		}
+		fmt.Println("====")
+		fmt.Println("Service statuses:")
+		for host, serviceMap := range servicestatuses {
+			for service, serviceStatus := range serviceMap {
+				fmt.Println("  ", host, service+":", serviceStatus)
+			}
+		}
+	}
+}
